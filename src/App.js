a255a005bc711aa6106f1bd3f57ba652d90e34df
alexis-tao <tao.alexis@gmail.com>
2019-10-14 21:48:28 -0400

    init design

--- src/App.js
+++ src/App.js
@@ -15,18 +15,18 @@ const Text = styled.p`
   font-size: 2em;
 `;
 
-const Content = styled.div`
+const TextWrapper = styled.div`
   width: 100%;
+  margin-top: 2em;
 `;
 
 // to do: fade in animation
-const PageWrapper = styled.div`
-  max-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: left;
-  margin: 5em;
+const Content = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 70%;
+  padding: 2em;
 `;
 
 const ImageWrapper = styled.div`
@@ -34,22 +34,17 @@ const ImageWrapper = styled.div`
   flex-direction: row;
   flex-wrap: wrap;
   width: 100%;
-  justify-content: space-evenly;
+  justify-content: space-around;
 `;
 
 const Overlay = styled.div`
   background-image: url(${props => props.src});
   background-repeat: no-repeat;
   background-size: contain;
-  width: 20em;
-  height: 20em;
+  width: 22em;
+  height: 22em;
   margin: 0.5em;
   cursor: pointer;
-`;
-
-const SmallOverlay = styled(Overlay)`
-  width: 15em;
-  height: 15em;
 `;
 
 const OverlayText = styled.p`
@@ -69,34 +64,54 @@ const OverlayText = styled.p`
   }
 `;
 
-const Header = styled.div`
+const SideBar = styled.div`
+  background-color: #dfcdc3;
   display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 2em;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 20%;
+  text-align: right;
+  height: fill-available;
+`;
+
+const IconWrapper = styled.a`
+  height: auto;
+  cursor: pointer;
   margin: 0.5em;
 `;
 
-const SideBar = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 20em;
-  height: 100%;
-  background-color: #dfcdc3;
+const Icon = styled.img`
+  width: 1.8em;
+  height: auto;
+`;
+
+const Icons = styled.div`
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
   align-items: center;
+  justify-content: flex-end;
+`;
+
+const Page = styled.div`
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
 `;
 
 export class App extends React.Component {
   render() {
     return (
-      <PageWrapper>
-        {/* <SideBar></SideBar> */}
-        <Header>
-          <Content>
+      <Page>
+        <SideBar>
+          <TextWrapper>
             <Text>
               <Typist>
                 <Typist.Delay ms={500} />
@@ -105,23 +120,47 @@ export class App extends React.Component {
             </Text>
             <SmallText>3rd year computer science student</SmallText>
             <SmallText>university of waterloo</SmallText>
-          </Content>
-        </Header>
-        <ImageWrapper>
-          <Overlay src={meImg}>
-            <OverlayText>about me</OverlayText>
-          </Overlay>
-          <Overlay src={eduImg}>
-            <OverlayText>education</OverlayText>
-          </Overlay>
-          <SmallOverlay src={osloImg}>
-            <OverlayText>experience</OverlayText>
-          </SmallOverlay>
-          <Overlay src={foodImg}>
-            <OverlayText>hobbies</OverlayText>
-          </Overlay>
-        </ImageWrapper>
-      </PageWrapper>
+          </TextWrapper>
+          <Icons>
+            <IconWrapper target="_blank" href="https://github.com/alexis-tao">
+              <Icon
+                src="https://image.flaticon.com/icons/svg/25/25231.svg"
+                alt="Github"
+              />
+            </IconWrapper>
+            <IconWrapper
+              target="_blank"
+              href="https://linkedin.com/in/alexistao/"
+              alt="LinkedIn"
+            >
+              <Icon src="https://cdn3.iconfinder.com/data/icons/picons-social/57/11-linkedin-512.png" />
+            </IconWrapper>
+            <IconWrapper
+              target="_blank"
+              href="mailto:tao.alexis@gmail.com"
+              alt="Email"
+            >
+              <Icon src="https://cdn4.iconfinder.com/data/icons/aiga-symbol-signs/439/aiga_mail-512.png" />
+            </IconWrapper>
+          </Icons>
+        </SideBar>
+        <Content>
+          <ImageWrapper>
+            <Overlay src={meImg}>
+              <OverlayText>about me</OverlayText>
+            </Overlay>
+            <Overlay src={eduImg}>
+              <OverlayText>education</OverlayText>
+            </Overlay>
+            <Overlay src={osloImg}>
+              <OverlayText>experience</OverlayText>
+            </Overlay>
+            <Overlay src={foodImg}>
+              <OverlayText>hobbies</OverlayText>
+            </Overlay>
+          </ImageWrapper>
+        </Content>
+      </Page>
     );
   }
 }
